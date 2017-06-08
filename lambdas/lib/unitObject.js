ae2a3a7749b70690d6159e7e7e23f17af39aac1f
Benze Gong <benzebgong@gmail.com>
2017-06-07 17:59:33 -0700

    add measure unit reference and object init function

---
+++ lambdas/lib/unitObject.js
@@ -0,0 +1,50 @@
+const unitLibrary = { 
+	length: {
+		metric: {
+			base: 'meter',
+			meter: 1,
+			coeff: 'base10',
+			micrometer: -6,
+			millimeter: -3,
+			centimeter: -2,
+			decimeter: -1,
+			kilometer: 3,
+		},
+		imperial: {
+			base: 'foot',
+			coeff: 'fixed',
+			foot: 1,
+			inch: 1 / 12,
+			yard: 3,
+			mile: 5280
+		},
+		conversionRate: {
+			metricImperial: 3.28084
+		}
+	}
+};
+
+const familyIdentifier = {
+	meter: {family: 'length', system: 'metric'},
+	micrometer: {family: 'length', system: 'metric'},
+	millimeter: {family: 'length', system: 'metric'},
+	centimeter: {family: 'length', system: 'metric'},
+	decimeter: {family: 'length', system: 'metric'},
+	kilometer: {family: 'length', system: 'metric'},
+	foot: {family: 'length', system: 'imperial'},
+	inch: {family: 'length', system: 'imperial'},
+	yard: {family: 'length', system: 'imperial'},
+	mile: {family: 'length', system: 'imperial'}
+}
+
+const build = (unit, val) => {
+	const unitObj = {};
+	unitObj.unit = unit;
+	unitObj.val = val ? parseFloat(val): 0;
+	unitObj.val = parseFloat(val);
+	unitObj.meta= familyIdentifier[unit];
+	unitObj.system = unitLibrary[unitObj.meta.family][unitObj.meta.system];
+	return unitObj;
+}
+
+export { unitLibrary, familyIdentifier, build };
